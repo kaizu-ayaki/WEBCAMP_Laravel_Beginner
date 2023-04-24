81aea6138190bf6b3d5e6f83685c354aebee271a
海津彩生 <kaizu.ayaki@example.com>
2023-04-24 00:46:40 +0000

    管理画面デザインにBootstrap5を入れた

--- resources/views/admin/user/list.blade.php
+++ resources/views/admin/user/list.blade.php
@@ -5,14 +5,14 @@
     <h1>ユーザー一覧</h1>
     <table border="1">
         <tr>
-            <th>ユーザーID
-            <th>ユーザー名
+            <th>ユーザID
+            <th>ユーザ名
             <th>タスク件数
-    @foreach($users as $user)
+@foreach ($users as $user)
         <tr>
-            <td>{{$user->id}}
-            <td>{{$user->name}}
-            <td>{{$user->task_num}}
-    @endforeach
-    </table>
+            <td>{{ $user->id }}
+            <td>{{ $user->name }}
+            <td>{{ $user->task_num }}
+@endforeach
+        </table>
 @endsection
